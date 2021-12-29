c97df56da16077d3a0f57ed882190ee4b907548f
bshafranek <60665227+bshafranek@users.noreply.github.com>
2021-12-29 12:12:07 -0700

    Create CreateProfile.py

---
+++ CreateProfile.py
@@ -0,0 +1,16 @@
+from arcgis.gis import GIS
+
+# BOGS Portals
+# Production
+# gis = GIS(r"https://biamaps.doi.gov/portal", r"BIA_GIS_Admin", r"Get Password From Vault", profile='portal_admin')
+
+# Internal Production
+# gis = GIS(r"https://biamaps.bc.doi.net/portal", r"BIA_GIS_Admin", r"Get Password From Vault", profile='internal_portal_admin')
+
+# Test
+# gis = GIS(r"https://testbiamaps.bc.doi.net/portal", r"BIA_GIS_Admin", r"Get Password From Vault", profile='test_portal_admin')
+
+# AGOL
+# gis = GIS(r"https://bia-geospatial.maps.arcgis.com", r"BIA_AGOL_Admin", r"Get Password From Vault", profile='AGOL_admin')
+
+print("Done")
